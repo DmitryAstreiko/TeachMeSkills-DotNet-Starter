65ff412394dbe5560c46052a019ebc117c0b2ed3
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-16 01:43:59 +0300

    feat: study \n. \t. \a

---
+++ src/TeachMeSkills.DotNet.Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first string : ");
+            var firstStr = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.Write("Enter second string : ");
+            var secondStr = Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.Write("Enter third string : ");
+            var thirdStr = Console.ReadLine();
+
+            Console.WriteLine($"\n{firstStr} \t{secondStr} \a{thirdStr}");
+
+            Console.ReadKey();
+        }
+    }
+}
